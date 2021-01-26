740d453de9537f462b49471e988617b20d9ae927
prs-watch <scarsdaleny15@yahoo.co.jp>
2021-01-26 23:31:28 +0900

    ~init

---
+++ src/SABR.jl
@@ -0,0 +1,109 @@
+module SABR
+
+export basehit, avg, obp, slg, ops, iso, rc, lazyrc, rc27, babip, woba, wraa, wrc, era, whip, fip, k9, bb9, h9, hr9
+
+# --- consts ---
+
+const DIGITS = 3
+
+# --- 共通function ---
+
+function sabrround(num)
+    return round(num, RoundNearestTiesAway, digits=DIGITS)
+end
+
+# --- バッティングfunction ---
+
+function basehit(hits, double, triple, hr)
+    return (hits - double - triple - hr)
+end
+
+function avg(hits, ab)
+    return sabrround(hits / ab)
+end
+
+function obp(hits, bb, hbp, ab, sf)
+    return sabrround((hits + bb + hbp) / (ab + bb + hbp + sf))
+end
+
+function slg(hits, double, triple, hr, ab)
+    return sabrround((basehit(hits, double, triple, hr) + 2 * double + 3 * triple + 4 * hr) / ab)
+end
+
+function ops(hits, double, triple, hr, bb, hbp, ab, sf)
+    return sabrround(
+        obp(hits, bb, hbp, ab, sf) + slg(hits, double, triple, hr, ab)
+    )
+end
+
+function iso(hits, double, triple, hr, ab)
+    return sabrround(
+        slg(hits, double, triple, hr, ab)- avg(hits, ab)
+    )
+end
+
+function rc(hits, double, triple, hr, bb, ab)
+    tb = basehit(hits, double, triple, hr) + 2 * double + 3 * triple + 4 * hr
+    return sabrround(((hits + bb) * tb) / (ab + bb))
+end
+
+function lazyrc(hits, double, triple, hr, bb, hbp, ab, sf)
+    tb = basehit(hits, double, triple, hr) + 2 * double + 3 * triple + 4 * hr
+    return sabrround(obp(hits, bb, hbp, ab, sf) * tb)
+end
+
+function rc27(hits, double, triple, hr, bb, ab, cs, sh, sf, dp)
+    return sabrround(rc(hits, double, triple, hr, bb, ab) * 27 / (ab - hits + cs + sh + sf + dp))
+end
+
+function babip(hits, hr, ab, k, sf)
+    return sabrround((hits - hr) / (ab - k - hr + sf))
+end
+
+function woba(hits, double, triple, hr, ab, bb, hbp, sf)
+    return sabrround(
+        ((bb + hbp) * 0.7 + 0.9 * basehit(hits, double, triple, hr) + 1.3 * double + 1.6 * triple + 2 * hr) / (ab + bb + hbp + sf)
+    )
+end
+
+function wraa(hits, double, triple, hr, ab, pa, bb, hbp, sf, lwoba)
+    return sabrround(((woba(hits, double, triple, hr, ab, bb, hbp, sf) - lwoba) / 1.2) * pa)
+end
+
+function wrc(hits, double, triple, hr, ab, pa, bb, hbp, sf, lwoba, lr, lpa)
+    return sabrround(
+        (((woba(hits, double, triple, hr, ab, bb, hbp, sf) - lwoba) / 1.2) + (lr / lpa)) * pa
+    )
+end
+
+# --- ピッチングfunction ---
+
+function era(er, ip)
+    return sabrround((er / ip) * 9)
+end
+
+function whip(hits, bb, ip)
+    return sabrround((hits + bb) / ip)
+end
+
+function fip(hr, bb, ibb, hbp, k, ip)
+    return sabrround(((13 * hr + 3 * (bb - ibb + hbp) - 2 * k) / ip) + 3)
+end
+
+function k9(k, ip)
+    return sabrround((k / ip) * 9)
+end
+
+function bb9(bb, ip)
+    return sabrround((bb / ip) * 9)
+end
+
+function h9(h, ip)
+    return sabrround((h / ip) * 9)
+end
+
+function hr9(hr, ip)
+    return sabrround((hr / ip) * 9)
+end
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,50 @@
+using Test
+using SABR
+
+# --- Mike Trout 2019 ---
+const PA = 600
+const AB = 470
+const HITS = 137
+const DOUBLE = 27
+const TRIPLE = 2
+const HR = 45
+const SB = 11
+const CS = 2
+const BB = 110
+const SO = 120
+const HBP = 16
+const DP = 5
+const SH = 0
+const SF = 4
+const IBB = 14
+
+@test basehit(HITS, DOUBLE, TRIPLE, HR) == 63
+@test avg(HITS, AB) == 0.291
+@test obp(HITS, BB, HBP, AB, SF) == 0.438
+@test slg(HITS, DOUBLE, TRIPLE, HR, AB) == 0.645
+@test ops(HITS, DOUBLE, TRIPLE, HR, BB, HBP, AB, SF) == 1.083
+@test iso(HITS, DOUBLE, TRIPLE, HR, AB) == 0.354
+@test rc(HITS, DOUBLE, TRIPLE, HR, BB, AB) == 129.036
+@test lazyrc(HITS, DOUBLE, TRIPLE, HR, BB, HBP, AB, SF) == 132.714
+@test rc27(HITS, DOUBLE, TRIPLE, HR, BB, AB, CS, SH, SF, DP) == 10.128
+@test babip(HITS, HR, AB, SO, SF) == 0.298
+@test woba(HITS, DOUBLE, TRIPLE, HR, AB, BB, HBP, SF) == 0.455
+@test wraa(HITS, DOUBLE, TRIPLE, HR, AB, PA, BB, HBP, SF, 0.33) == 62.5
+
+# --- Gerritt Cole 2019 ---
+const ER = 59
+const IP = 212.1
+const K = 326
+const PBB = 48
+const PIBB = 0
+const PHBP = 3
+const PHR = 29
+const PH = 142
+
+@test era(ER, IP) == 2.504
+@test whip(PH, PBB, IP) == 0.896
+@test fip(PHR, PBB, PIBB, PHBP, K, IP) == 2.425
+@test k9(K, IP) == 13.833
+@test bb9(PBB, IP) == 2.037
+@test h9(PH, IP) == 6.025
+@test hr9(PHR, IP) == 1.231
